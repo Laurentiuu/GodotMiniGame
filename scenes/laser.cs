106f61586486bae47d4b4f44a009b62404ea93a2
Laurentiuu <laurgales@yahoo.com>
2024-07-23 23:44:32 +0300

    add game over scene

--- scenes/laser.cs
+++ scenes/laser.cs
@@ -10,6 +10,10 @@ public partial class laser : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Sprite2D sprite = GetNode<Sprite2D>("Sprite2D");
+		sprite.Scale = new Vector2(0, 0);
+		var tween = CreateTween();
+		tween.TweenProperty(sprite, "scale", new Vector2(1, 1), 0.2);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
